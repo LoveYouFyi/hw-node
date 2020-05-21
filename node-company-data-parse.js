31659ec8834bcf226483ad53de191b2192b0887d
NEH <erik@bodigiti.com>
2020-05-21 18:08:01 -0400

    read: read: added new object format records to node-company-data-parse.js including duplicates. # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # Date: Thu May 21 18:08:01 2020 -0400 # # On branch read # Changes to be committed: # modified: node-company-data-parse.js #

--- node-company-data-parse.js
+++ node-company-data-parse.js
@@ -2,60 +2,97 @@
 const data = 
   [
     {
-      row: {
-        user_id: 'mfattori0',
-        first_name: 'Mariya',
-        last_name: 'Fattori',
-        version: '5',
-        insurance_company: 'Ledner'
-      }
+      user_id: 'mfattori0',
+      first_name: 'Mariya',
+      last_name: 'Fattori',
+      version: '5',
+      insurance_company: 'Ledner'
     },
     {
-      row: {
-        user_id: 'kjeavons1',
-        first_name: 'Kelsey',
-        last_name: 'Jeavons',
-        version: '9',
-        insurance_company: 'Ledner'
-      }
+      user_id: 'kjeavons1',
+      first_name: 'Kelsey',
+      last_name: 'Jeavons',
+      version: '9',
+      insurance_company: 'Ledner'
     },
     {
-      row: {
-        user_id: 'xfeldklein2',
-        first_name: 'Xerxes',
-        last_name: 'Feldklein',
-        version: '5',
-        insurance_company: 'Pfannerstill'
-      }
+      user_id: 'xfeldklein2',
+      first_name: 'Xerxes',
+      last_name: 'Feldklein',
+      version: '5',
+      insurance_company: 'Ledner'
     },
     {
-      row: {
-        user_id: 'jstannering3',
-        first_name: 'Jed',
-        last_name: 'Stannering',
-        version: '6',
-        insurance_company: 'Pfannerstill'
-      }
+      user_id: 'xfeldklein2',
+      first_name: 'Xerxes',
+      last_name: 'Feldklein',
+      version: '9',
+      insurance_company: 'Ledner'
     },
     {
-      row: {
-        user_id: 'aadderleyt',
-        first_name: 'Abe',
-        last_name: 'Adderley',
-        version: '8',
-        insurance_company: 'Weber-Bradtke'
-      }
+      user_id: 'mpagden4',
+      first_name: 'Miranda',
+      last_name: 'Pagden',
+      version: '6',
+      insurance_company: 'Pfannerstill'
     },
     {
-      row: {
-        user_id: 'fjanicu',
-        first_name: 'Free',
-        last_name: 'Janic',
-        version: '2',
-        insurance_company: 'Weber-Bradtke'
-      }
+      user_id: 'cdrewe5',
+      first_name: 'Cathy',
+      last_name: 'Drewe',
+      version: '7',
+      insurance_company: 'Pfannerstill'
+    },
+    {
+      user_id: 'hroose6',
+      first_name: 'Harley',
+      last_name: 'Roose',
+      version: '3',
+      insurance_company: 'Pfannerstill'
+    },
+    {
+      user_id: 'mnursey7',
+      first_name: 'Malissa',
+      last_name: 'Nursey',
+      version: '9',
+      insurance_company: 'Pfannerstill'
+    },
+    {
+      user_id: 'jellerey8',
+      first_name: 'Jacquelin',
+      last_name: 'Ellerey',
+      version: '3',
+      insurance_company: 'Orn'
+    },
+    {
+      user_id: 'jmarlow9',
+      first_name: 'Jewel',
+      last_name: 'Marlow',
+      version: '6',
+      insurance_company: 'Orn'
+    },
+    {
+      user_id: 'mpagden4',
+      first_name: 'Miranda',
+      last_name: 'Pagden',
+      version: '9',
+      insurance_company: 'Orn'
+    },
+    {
+      user_id: 'mpagden4',
+      first_name: 'Miranda',
+      last_name: 'Pagden',
+      version: '4',
+      insurance_company: 'Orn'
+    },
+    {
+      user_id: 'ageneryb',
+      first_name: 'Alfy',
+      last_name: 'Genery',
+      version: '1',
+      insurance_company: 'Orn'
     }
-  ]
+    ]
 
 // argument 'propKey' value must be of type 'string' or 'number'
 const sortObjectsAsc = (array, propKey) => array.sort((a, b) => {
@@ -72,3 +109,4 @@ const objectValuesByKey = array => propKey => array.reduce((a, c) => {
   a.push(c[propKey]);
   return a;
 }, []);
+
