22d8d65da94a6a59e24c9352434e703f03239a93
NEH <erik@bodigiti.com>
2020-05-22 03:09:42 -0400

    write-parsed-csv--duplicates-not-removed-from-written: Fixed remove duplicates so the array's index that is removed is removed from the intended indexes instead of previous version where map was used an each iteration reset the index.

--- app-utility.js
+++ app-utility.js
@@ -1,5 +1,22 @@
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
+const writeCSV = (data, name) => {
+  const csvWriter = createCsvWriter({
+    path: `./parsed-files/${name}.csv`,
+    header: [
+      {id: 'user_id', title: 'user_id'},
+      {id: 'first_name', title: 'first_name'},
+      {id: 'last_name', title: 'last_name'},
+      {id: 'version', title: 'version'},
+      {id: 'insurance_company', title: 'insurance_company'},
+    ]
+  });
+
+  csvWriter
+    .writeRecords(data)
+    .then(()=> console.log('The CSV file was written successfully'));
+}
+
 const distinctCompanyNames = (array, propType) => [...new Set(array.map(prop => prop[propType]))];
 
 const distinctCompanyRows = (array, name) => array.filter(row => row.insurance_company === name);
@@ -26,56 +43,46 @@ const duplicatesFlagLowVersionToRemove = (array, indexesToRemove) => {
     if (a.user_id === b.user_id) {
       if (a.version <= b.version) {
         const indexA = array.indexOf(a);
-        console.log("a: ", indexA, a)
+//        console.log("a: ", indexA, a)
         indexesRemove.push(indexA);
       }
       if (a.version >= b.version) {
         const indexB = array.indexOf(b);
-        console.log("b: ", indexB, b)
+//        console.log("b: ", indexB, b)
         indexesRemove.push(indexB);
       }
     }
 
   });
-  console.log("indexexRemove 4444444444444444444444444 ", indexesRemove);
+//  console.log("indexexRemove 4444444444444444444444444 ", indexesRemove);
   return indexesRemove;
 }
 
-const arrayRemoveDuplicates = (arrayToMap, arrayToTrim) => arrayToMap.map((e) => {
-  return arrayToTrim.splice(e, 1);
+//const arrayRemoveDuplicates = (arrayToMap, arrayToTrim) => arrayToMap.map((e) => {
+  //return arrayToTrim.splice(e, 1);
+//});
+
+const newData = (array, removeValFrom) => array.filter(function(value, index) {
+  return removeValFrom.indexOf(index) == -1;
 });
 //
 // Duplicates Removal
 ////////////////////////////////////////////////////////////////////////////////
 
-const writeCSV = (data, name) => {
-  const csvWriter = createCsvWriter({
-    path: `./parsed-files/${name}.csv`,
-    header: [
-      {id: 'user_id', title: 'user_id'},
-      {id: 'first_name', title: 'first_name'},
-      {id: 'last_name', title: 'last_name'},
-      {id: 'version', title: 'version'},
-      {id: 'insurance_company', title: 'insurance_company'},
-    ]
-  });
-
-  csvWriter
-    .writeRecords(data)
-    .then(()=> console.log('The CSV file was written successfully'));
-}
 
 const rowsByCompany = (data) => 
   distinctCompanyNames(data, 'insurance_company').map(name => {
-    const indexesToRemove = [];
+//    const indexesToRemove = [];
     const filtered = distinctCompanyRows(data, name);
     const sorted = sortByLastThenFirstName(filtered);
-    duplicatesFlagLowVersionToRemove(sorted, indexesToRemove);
-    console.log("Here $$$$$$$$$$$$$$$$ ", duplicatesFlagLowVersionToRemove(sorted, indexesToRemove));
-    console.log("sorted: ", sorted);
+    const indexesToRemove = duplicatesFlagLowVersionToRemove(sorted);
     console.log("indexesToRemove: ", indexesToRemove);
-    arrayRemoveDuplicates(indexesToRemove, sorted);
-    return writeCSV(sorted, name);
+//    const removed = arrayRemoveDuplicates(indexesToRemove, sorted);
+//    console.log("removed: %%%%%%%%%%%%%%%%% ", removed);
+//    console.log("sorted: ", sorted);
+    const final = newData(sorted, indexesToRemove);
+    console.log("final $$$$$$$$$$$$$$$$$$ ", final);
+    return writeCSV(final, name);
   });
 
 exports.parse = rowsByCompany;
