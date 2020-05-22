efa1fc6f3ee457448326b64933289bb4f9f15de2
NEH <erik@bodigiti.com>
2020-05-22 03:49:16 -0400

    write-parsed-csv--duplicates-not-removed-from-written: renamed some vars.

--- app-utility.js
+++ app-utility.js
@@ -1,22 +1,5 @@
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
-const writeCSV = (data, name) => {
-  const csvWriter = createCsvWriter({
-    path: `./parsed-files/${name}.csv`,
-    header: [
-      {id: 'user_id', title: 'user_id'},
-      {id: 'last_name', title: 'last_name'},
-      {id: 'first_name', title: 'first_name'},
-      {id: 'version', title: 'version'},
-      {id: 'insurance_company', title: 'insurance_company'},
-    ]
-  });
-
-  csvWriter
-    .writeRecords(data)
-    .then(()=> console.log('The CSV file was written successfully'));
-}
-
 const distinctCompanyNames = (array, propType) => [...new Set(array.map(prop => prop[propType]))];
 
 const distinctCompanyRows = (array, name) => array.filter(row => row.insurance_company === name);
@@ -32,10 +15,8 @@ const sortByLastThenFirstName = (array) => array.sort((a, b) => {
   return 0; // if equal
 });
 
-////////////////////////////////////////////////////////////////////////////////
-// Duplicates Removal
-//
-const duplicatesFlagLowVersionToRemove = (array, indexesToRemove) => {
+// Identify duplicates to be removed by array index
+const duplicatesFlagLowVersionToRemove = (array) => {
   const indexesRemove = [];
 
   array.sort((a, b) => {
@@ -45,42 +26,53 @@ const duplicatesFlagLowVersionToRemove = (array, indexesToRemove) => {
     if (a.user_id === b.user_id) {
       if (versionA <= versionB) {
         const indexA = array.indexOf(a);
-//        console.log("a: ", indexA, a)
         indexesRemove.push(indexA);
       }
       if (versionA >= versionB) {
         const indexB = array.indexOf(b);
-//        console.log("b: ", indexB, b)
         indexesRemove.push(indexB);
       }
     }
 
   });
-//  console.log("indexexRemove 4444444444444444444444444 ", indexesRemove);
   return indexesRemove;
 }
 
-//const arrayRemoveDuplicates = (arrayToMap, arrayToTrim) => arrayToMap.map((e) => {
-  //return arrayToTrim.splice(e, 1);
-//});
-
-const newData = (array, removeValFrom) => array.filter(function(value, index) {
+// Return new array after removing duplicates by index
+const dataDuplicatesRemoved = (array, removeValFrom) => array.filter(function(value, index) {
   return removeValFrom.indexOf(index) == -1;
 });
-//
-// Duplicates Removal
-////////////////////////////////////////////////////////////////////////////////
 
-
-const rowsByCompany = (data) => 
-  distinctCompanyNames(data, 'insurance_company').map(name => {
-    const filtered = distinctCompanyRows(data, name);
-    const sorted = sortByLastThenFirstName(filtered);
-    const indexesToRemove = duplicatesFlagLowVersionToRemove(sorted);
-    const parsedData = newData(sorted, indexesToRemove);
-    const sortFinal = sortByLastThenFirstName(parsedData);
-    return writeCSV(sortFinal, name);
+// Write CSV files for data by company name
+const writeCSV = (data, company) => {
+  const csvWriter = createCsvWriter({
+    path: `./parsed-files/${company}.csv`,
+    header: [
+      {id: 'user_id', title: 'user_id'},
+      {id: 'last_name', title: 'last_name'},
+      {id: 'first_name', title: 'first_name'},
+      {id: 'version', title: 'version'},
+      {id: 'insurance_company', title: 'insurance_company'},
+    ]
   });
 
-exports.parse = rowsByCompany;
+  csvWriter
+    .writeRecords(data)
+    .then(()=> console.log('The CSV file was written successfully'));
+}
+
+// Parse and Write by Company 
+// for duplicate user_id keep only the one with highest version 
+// sort by last name, first name 
+const parseAndWriteByCompany = (data) => 
+  distinctCompanyNames(data, 'insurance_company').map(company => {
+    const filtered = distinctCompanyRows(data, company);
+    const dataSortedOne = sortByLastThenFirstName(filtered);
+    const indexesToRemove = duplicatesFlagLowVersionToRemove(dataSortedOne);
+    const parsedData = dataDuplicatesRemoved(dataSortedOne, indexesToRemove);
+    const dataSortedTwo = sortByLastThenFirstName(parsedData);
+    return writeCSV(dataSortedTwo, company);
+  });
+
+exports.parse = parseAndWriteByCompany;
 
